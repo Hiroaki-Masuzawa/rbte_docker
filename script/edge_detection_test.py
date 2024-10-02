1f5fc3c3035a4a945cad35a43a0b45b5af9c2ce4
Hiroaki Masuzawa <masuzawa.hiroaki.vl@tut.jp>
2024-10-02 17:46:47 +0900

    fix test code

--- script/edge_detection_test.py
+++ script/edge_detection_test.py
@@ -12,7 +12,7 @@ sys.path.append('/userdir/BDCN')
 import bdcn
 
 sys.path.append('/userdir/hed')
-from run import Network
+from hed_network import Network
 
 
 sys.path.append('/userdir/im2rbte')
@@ -48,8 +48,8 @@ def detect_hed_edge(model, image):
 
     assert(intWidth == 480) # remember that there is no guarantee for correctness, comment this line out if you acknowledge this and want to continue
     assert(intHeight == 320) # remember that there is no guarantee for correctness, comment this line out if you acknowledge this and want to continue
-
-    tenOutput = model(tenInput.view(1, 3, intHeight, intWidth))[0, :, :, :]
+    with torch.no_grad():
+        tenOutput = model(tenInput.view(1, 3, intHeight, intWidth))[0, :, :, :]
     tenOutput = torchvision.transforms.functional.resize(img=tenOutput, size=(raw_height, raw_width))
     return tenOutput.clip(0.0, 1.0).numpy().transpose(1, 2, 0)[:, :, 0]
 
